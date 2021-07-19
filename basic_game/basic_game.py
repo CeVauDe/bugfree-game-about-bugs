95a442bd24eca54e9f0dbc2b1ec4612c694d13c4
CeVauDe <74298739+CeVauDe@users.noreply.github.com>
2021-07-19 22:09:01 +0200

    Calculate distance to background from endge of the block instead of the center

--- basic_game/basic_game.py
+++ basic_game/basic_game.py
@@ -2,6 +2,7 @@ import os
 import math
 import pygame
 import sys
+import numpy as np
 
 
 def simulate():
@@ -30,7 +31,11 @@ def simulate():
 def update():
     pos = [[center[0], center[1]], [center[0], center[1]], [center[0], center[1]], [center[0], center[1]],
            [center[0], center[1]], [center[0], center[1]], [center[0], center[1]], [center[0], center[1]]]
+    pos2 = [[center[0], center[1]]] * 8  # Python magic :o
+    pos3 = np.array([center] * 8, dtype=float)  # even more magic
+    pos3 += block_offset
 
+    pos = pos3.astype(int)
     pygame.font.init()
     textfont = pygame.font.SysFont('Arial', 30)
 
@@ -38,12 +43,15 @@ def update():
         length = 0
         while not surface.get_at((pos[i][0], pos[i][1])) == BORDER_COLOR and length < 2000:
             length = length + 1
-            pos[i][0] = int(center[0] + math.cos(math.radians(360 - degree)) * length * pos_fak[i][0])
-            pos[i][1] = int(center[1] + math.sin(math.radians(360 - degree)) * length * pos_fak[i][1])
+            # pos[i][0] = int(center[0] + math.cos(math.radians(360 - degree)) * length * pos_fak[i][0])
+            # pos[i][1] = int(center[1] + math.sin(math.radians(360 - degree)) * length * pos_fak[i][1])
+            pos[i, :] += pos_fak[i, :]
 
-        pygame.draw.line(surface, (0, 0, 255), (center[0], center[1]), (pos[i][0], pos[i][1]))
+        # pygame.draw.line(surface, (0, 0, 255), (center[0], center[1]), (pos[i][0], pos[i][1]))
+        pygame.draw.line(surface, (0, 0, 255), pos3[i, :], pos[i, :])
 
-        dist[i] = int(math.sqrt(math.pow(pos[i][0] - center[0], 2) + math.pow(pos[i][1] - center[1], 2)))
+        # dist[i] = int(math.sqrt(math.pow(pos[i][0] - center[0], 2) + math.pow(pos[i][1] - center[1], 2)))
+        dist[i] = int(math.sqrt(np.sum(np.square(pos[i, :] - pos3[i, :]))))
         surface.blit(textfont.render(str(dist[i]), False, (0, 0, 0)), (pos[i][0] + offset[i][0], pos[i][1] + offset[i][1]))
 
     surface.blit(blockImage, (center[0] - 25, center[1] - 25))
@@ -80,9 +88,12 @@ if __name__ == "__main__":
     BORDER_COLOR = (255, 255, 255, 255)
 
     degree = 45
-    pos_fak = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-    dist = [0, 0, 0, 0, 0, 0, 0, 0]
-    offset = [[-20, -25], [-20, -25], [10, -25], [-20, 0], [-20, 0], [-20, 0], [-40, -25], [-20, -25]]
+    pos_fak = np.array([[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]])
+    block_offset = np.zeros_like(pos_fak)
+    block_offset[:, 0] = pos_fak[:, 0] * block_width / 2
+    block_offset[:, 1] = pos_fak[:, 1] * block_height / 2
+    dist = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+    offset = np.array([[-20, -25], [-20, -25], [10, -25], [-20, 0], [-20, 0], [-20, 0], [-40, -25], [-20, -25]])
 
     while True:
         simulate()
