f2d90bb49724038563c0a4c46300989ab3562d03
bele255 <78923336+bele255@users.noreply.github.com>
2021-07-19 19:55:16 +0200

    changed model so arrow keys move and rotate model

--- basic_game/basic_game.py
+++ basic_game/basic_game.py
@@ -5,29 +5,6 @@ import sys
 
 
 def simulate():
-
-    surface.fill(White)
-    surface.blit(displayImage, (0, 0))
-    for eve in pygame.event.get():
-        if eve.type==pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    key_input = pygame.key.get_pressed()
-    if key_input[pygame.K_LEFT]:
-        center[0] -= step
-    if key_input[pygame.K_UP]:
-        center[1] -= step
-    if key_input[pygame.K_RIGHT]:
-        center[0] += step
-    if key_input[pygame.K_DOWN]:
-        center[1] += step
-
-    update()
-    fps_clock.tick(fps)
-
-
-def update():
     pos = [[center[0], center[1]], [center[0], center[1]], [center[0], center[1]], [center[0], center[1]],
            [center[0], center[1]], [center[0], center[1]], [center[0], center[1]], [center[0], center[1]]]
 
@@ -36,7 +13,7 @@ def update():
 
     for i in range(0, 8):
         length = 0
-        while not surface.get_at((pos[i][0], pos[i][1])) == BORDER_COLOR and length < 2000:
+        while not surface.get_at(pos[i]) == BORDER_COLOR and length < 2000:
             length = length + 1
             pos[i][0] = int(center[0] + math.cos(math.radians(360 - degree)) * length * pos_fak[i][0])
             pos[i][1] = int(center[1] + math.sin(math.radians(360 - degree)) * length * pos_fak[i][1])
@@ -46,10 +23,9 @@ def update():
         dist[i] = int(math.sqrt(math.pow(pos[i][0] - center[0], 2) + math.pow(pos[i][1] - center[1], 2)))
         surface.blit(textfont.render(str(dist[i]), False, (0, 0, 0)), (pos[i][0] + offset[i][0], pos[i][1] + offset[i][1]))
 
-    surface.blit(blockImage, (center[0] - 25, center[1] - 25))
-
+    rot_Image = pygame.transform.rotate(blockImage, winkel)
+    surface.blit(rot_Image, (center[0] - int(rot_Image.get_width()/2), center[1] - int(rot_Image.get_height()/2)))
     pygame.display.update()
-
 
 if __name__ == "__main__":
 
@@ -83,6 +59,28 @@ if __name__ == "__main__":
     pos_fak = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
     dist = [0, 0, 0, 0, 0, 0, 0, 0]
     offset = [[-20, -25], [-20, -25], [10, -25], [-20, 0], [-20, 0], [-20, 0], [-40, -25], [-20, -25]]
+    winkel = 0
 
     while True:
+        surface.fill(White)
+        surface.blit(displayImage, (0, 0))
+        for eve in pygame.event.get():
+            if eve.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        key_input = pygame.key.get_pressed()
+        if key_input[pygame.K_LEFT]:
+            winkel += step
+        if key_input[pygame.K_UP]:
+            center[0] += int(math.cos(math.radians(270 - winkel)) * step)
+            center[1] += int(math.sin(math.radians(270 - winkel)) * step)
+        if key_input[pygame.K_RIGHT]:
+            winkel -= step
+        if key_input[pygame.K_DOWN]:
+            center[0] -= int(math.cos(math.radians(270 - winkel)) * step)
+            center[1] -= int(math.sin(math.radians(270 - winkel)) * step)
+
+        pygame.draw.circle(surface, (0,0,255), center, 2)
         simulate()
+        fps_clock.tick(fps)
